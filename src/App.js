9b6a9a5ea71d20ecf6c4c2968b562ec1c5aced40
Mariana Merlim Rodrigues <104798332+Marimerlim@users.noreply.github.com>
2022-07-18 14:50:55 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// Crie um Component de function
+//_e retorne uma frase na tela
+
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <h1>Prática React 03</h1>
+      <p>*Socorro Deus</p>
+    </>
+  );
+}
